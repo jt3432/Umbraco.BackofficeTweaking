a360163f2ce3014f985768d5263fa132044a821f
jt3432 <jt3432@gmail.com>
2016-02-24 11:32:30 -0700

    Add SimpleRichTextEditor to collection.

---
+++ src/Axial.Umbraco.SimpleRichTextEditor/Install/PluginInstaller.cs
@@ -0,0 +1,56 @@
+﻿using Axial.Umbraco.SimpleRichTextEditor.Utilities;
+using System.IO;
+using System.Web;
+using System.Xml;
+
+namespace Axial.Umbraco.SimpleRichTextEditor.Install
+{
+    public static class PluginInstaller
+    {
+        #region Constants
+
+        private const string APP_VERSION_KEY = "SimpleRichTextEditor:Version";
+        private const string APP_VERSION_VALUE = "1.0";
+
+        #endregion
+
+        #region Private Variables
+
+        #endregion
+
+        #region Public Methods
+
+        public static void InstallAppSettingKeys()
+        {
+            if (PluginHelpers.ReadSetting(APP_VERSION_KEY).Equals("Not Found"))
+            {
+                PluginHelpers.AddUpdateAppSettings(APP_VERSION_KEY, "Install");
+            }
+        }
+
+        public static void InstallFiles()
+        {
+            if (!PluginHelpers.ReadSetting(APP_VERSION_KEY).Equals(APP_VERSION_VALUE))
+            {
+                InstallPluginFiles();
+                PluginHelpers.AddUpdateAppSettings(APP_VERSION_KEY, APP_VERSION_VALUE);
+            }
+        }
+        
+        #endregion
+
+        #region Private Methods
+
+        private static void InstallPluginFiles()
+        {
+            if (Directory.Exists(@"~\App_Plugins\SimpleRichTextEditor"))
+            {
+                Directory.Delete(HttpContext.Current.Server.MapPath(@"~\App_Plugins\SimpleRichTextEditor"), true);
+            }
+            PluginHelpers.ExtractEmbeddedResources(@"Axial.Umbraco.SimpleRichTextEditor.Resources", @"App_Plugins");
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Axial.Umbraco.SimpleRichTextEditor/Resources/App_Plugins/SimpleRichTextEditor/controllers/simplerichtexteditor-propertyeditor-controller.js
@@ -0,0 +1,28 @@
+﻿
+angular.module('umbraco').controller('Umbraco.SimpleRichTextEditorController', ['$scope', '$log', '$timeout', function ($scope, $log, $timeout) {  
+
+    $scope.clean = function () {        
+        var regex = /(<([^>]+)>)/ig;
+        $scope.model.value = $('#' + $scope.model.alias).val().replace(regex, "");
+    };
+    $scope.bold = function () {
+        $scope.model.value = $('#' + $scope.model.alias).wrapText('<strong>', '</strong>');
+    };
+    $scope.italic = function () {
+        $scope.model.value = $('#' + $scope.model.alias).wrapText('<em>', '</em>');
+    };
+    $scope.strike = function () {
+        $scope.model.value = $('#' + $scope.model.alias).wrapText('<strike>', '</strike>');
+    };
+
+    $.fn.wrapText = function (openTag, closeTag) {
+        var $textField = this;
+        var len = $textField.val().length;
+        var start = $textField[0].selectionStart;
+        var end = $textField[0].selectionEnd;
+        var selectedText = $textField.val().substring(start, end);
+        var replacement = openTag + selectedText + closeTag;
+        return $textField.val().substring(0, start) + replacement + $textField.val().substring(end, len);
+    };
+
+}]);
