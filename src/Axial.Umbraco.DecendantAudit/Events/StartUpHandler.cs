40ca7c9bc962378670666cbbef189cf851e9858e
jason.thomas@axial.agency <jason.thomas@axial.agency>
2016-05-06 16:51:59 -0600

    refactored DecendantsAudit

---
+++ src/Axial.Umbraco.DecendantAudit/Events/StartUpHandler.cs
@@ -0,0 +1,18 @@
+﻿using Axial.Umbraco.DecendantsAudit.Install;
+using Umbraco.Core;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Trees;
+
+namespace Axial.Umbraco.DecendantsAudit
+{
+    public class StartUpHandlers : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            PluginInstaller.InstallAppSettingKeys();
+            PluginInstaller.InstallFiles();
+
+            base.ApplicationStarted(umbracoApplication, applicationContext);
+        }
+    }
+}
---
+++ src/Axial.Umbraco.DecendantAudit/Install/PluginInstaller.cs
@@ -0,0 +1,56 @@
+﻿using Axial.Umbraco.DecendantsAudit.Utilities;
+using System.IO;
+using System.Web;
+using System.Xml;
+
+namespace Axial.Umbraco.DecendantsAudit.Install
+{
+    public static class PluginInstaller
+    {
+        #region Constants
+
+        private const string APP_VERSION_KEY = "DecendantsAudit:Version";
+        private const string APP_VERSION_VALUE = "1.0";
+
+        #endregion
+
+        #region Private Variables
+
+        #endregion
+
+        #region Public Methods
+
+        public static void InstallAppSettingKeys()
+        {
+            if (PluginHelpers.ReadSetting(APP_VERSION_KEY).Equals("Not Found"))
+            {
+                PluginHelpers.AddUpdateAppSettings(APP_VERSION_KEY, "Install");
+            }
+        }
+
+        public static void InstallFiles()
+        {
+            if (!PluginHelpers.ReadSetting(APP_VERSION_KEY).Equals(APP_VERSION_VALUE))
+            {
+                InstallPluginFiles();
+                PluginHelpers.AddUpdateAppSettings(APP_VERSION_KEY, APP_VERSION_VALUE);
+            }
+        }
+        
+        #endregion
+
+        #region Private Methods
+
+        private static void InstallPluginFiles()
+        {
+            if (Directory.Exists(@"~\App_Plugins\DecendantsAudit"))
+            {
+                Directory.Delete(HttpContext.Current.Server.MapPath(@"~\App_Plugins\DecendantsAudit"), true);
+            }
+            PluginHelpers.ExtractEmbeddedResources(@"Axial.Umbraco.DecendantsAudit.Resources", @"App_Plugins");
+        }
+
+        #endregion
+
+    }
+}
